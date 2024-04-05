2ac62fecbfc5068e840893d4615f700f70022244
Tony Eder <tonyeder11@gmail.com>
2024-04-05 14:48:35 -0700

    good to go

--- server/models.py
+++ server/models.py
@@ -11,11 +11,9 @@ class Earthquake(db.Model, SerializerMixin):
     __tablename__ = 'earthquakes'
     
     id = db.Column(db.Integer, primary_key=True)
-    magnitude = db.column(db.Float)
-    location = db.column(db.String)
-    year = db.column(db.Integer)
+    magnitude = db.Column(db.Float)
+    location = db.Column(db.String)
+    year = db.Column(db.Integer)
 
     def __repr__(self):
         return f'<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>'
-
-
